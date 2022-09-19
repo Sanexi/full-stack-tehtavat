303db00a29809f547f15f6ff3883bbf97848e542
Santeri Salonen <santerisa.salonen@gmail.com>
2022-09-19 14:57:43 +0300

    Trying out code

--- osa1/part1/src/App.js
+++ osa1/part1/src/App.js
@@ -1,73 +1,27 @@
-const Header = (props) => {
-  console.log(props)
-  return <h1>{props.course}</h1>
-}
+import { useState } from 'react'
 
-const Content = (props) => {
-  console.log(props)
-  const p1 = props.parts[0]
-  const p2 = props.parts[1]
-  const p3 = props.parts[2]
-  console.log(p1)
-  return (
-    <div>
-      <p>
-      <Part part={p1.name} exercise={p1.exercises} />
-      <Part part={p2.name} exercise={p2.exercises} />
-      <Part part={p3.name} exercise={p3.exercises} />
-      </p>
-    </div>
-  )
-}
+const Display = props => <div>{props.value}</div>
 
-const Part = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-        {props.part} {props.exercise}
-      </p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  console.log(props)
-  const p1 = props.parts[0]
-  const p2 = props.parts[1]
-  const p3 = props.parts[2]
-  console.log(p1)
-  return (
-    <div>
-      <p>Number of exercises {p1.exercises + p2.exercises + p3.exercises}</p>
-    </div>
-  )
-}
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [value, setValue] = useState(10)
+
+  const setToValue = newValue => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts}/>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
